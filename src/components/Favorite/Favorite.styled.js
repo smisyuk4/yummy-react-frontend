e8c7e296b464bc9715d72af2eac7a78f9128ea40
Pobedynskyj Volodymyr <vp@readable.games>
2023-04-20 14:40:43 +0300

    create test favorite func

---
+++ src/components/Favorite/Favorite.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const FavoriteStyled = styled.div``;
+
+export const FavoriteTitle = styled.h1``;
+
+export const FavoriteList = styled.ul``;
+
+export const FavoriteItem = styled.li``;
