17052a1a4f61a300813b62b090f352345ebeba7c
OlexiiA <incretiblealex@gmail.com>
2023-04-21 22:25:10 +0300

    make hero

--- src/components/MainPage/Hero/Hero.jsx
+++ src/components/MainPage/Hero/Hero.jsx
@@ -1,13 +1,15 @@
-import { WrapperStyled, SectionStyled, DivStyled, DishStyled, TitleStyled, ParagrafStyled } from './Hero.styled';
+import { WrapperStyled, TopImgStyled, SectionStyled, DivStyled, InformFieldStyled, DishStyled, TitleStyled, ParagrafStyled, BoxStyled } from './Hero.styled';
 
 export const Hero = () => {
 	return <WrapperStyled>
-		<TitleStyled><span style={{color:'#8BAA36', textShadow: `0px 4px 4px rgba(0, 0, 0, 0.25)` }}>So</span>Yummy</TitleStyled>
+		<BoxStyled>
+			<TopImgStyled />
+			<TitleStyled><span style={{ color: '#8BAA36', textShadow: `0px 4px 4px rgba(0, 0, 0, 0.25)` }}>So</span>Yummy</TitleStyled>
+		</BoxStyled>
 		<ParagrafStyled>"What to cook?" is not only a recipe app, it is, in fact, your cookbook. You can add your own recipes to save them for the future.</ParagrafStyled>
-		<SectionStyled >
-		<DivStyled>
-			<DishStyled/>
-		</DivStyled>
-		</SectionStyled>
-		</WrapperStyled>;
+		<SectionStyled />
+		<DivStyled />
+		<DishStyled />
+		<InformFieldStyled/>
+	</WrapperStyled>;
 };
