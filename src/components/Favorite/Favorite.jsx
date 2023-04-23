1d0af162a52055c311441b63901c5d084427a832
Pobedynskyj Volodymyr <105819145+Pobedynskyj@users.noreply.github.com>
2023-04-24 01:35:59 +0300

    add correct way

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
-
 	FavoriteArticle,
 	FavoriteDeleteBtn,
 	FavoriteImg,
@@ -12,16 +12,34 @@ import {
 	FavoriteStyled,
 	FavoriteTime,
 	FavoriteTitle,
-
 } from './Favorite.styled';
+
+axios.defaults.baseURL = 'https://yummy-rest-api.onrender.com/';
+
+const TOKEN =
+	'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0NDE5OTNjODYzZWFmOWJiYTQ2YzY2NSIsImlhdCI6MTY4MjAyMDY2OCwiZXhwIjoxNjg0NjEyNjY4fQ.cc5YEHIsXZ2ZkIFUGCySz5LtjnoU7kjVbeYXtQpyCIw';
+
+const config = {
+	headers: {
+		auth: `${TOKEN}`,
+	},
+};
+
+export const fetchFav = async () => {
+	const responce = await axios.get(`recipes/favorite?auth=${TOKEN}`, config);
+	return responce;
+};
 
 export const Favorite = () => {
 	const [favoriteRecipes, setFavoriteRecipes] = useState();
 
+	console.log(favoriteRecipes);
+	console.log();
+
 	useEffect(() => {
-		fetch('https://yummy-rest-api.onrender.com/recipes/favorite/')
-			.then(response => response.json())
-			.then(data => setFavoriteRecipes(data));
+		fetchFav()
+			.then(({ data }) => setFavoriteRecipes(data))
+			.catch(error => error);
 	}, [setFavoriteRecipes]);
 
 	return (
@@ -29,23 +47,24 @@ export const Favorite = () => {
 			<FavoriteTitle>Favorites</FavoriteTitle>
 			<FavoriteList>
 				{favoriteRecipes !== null ??
-					favoriteRecipes.map(recipe => (
+					favoriteRecipes.map(favoriteRecipe => (
 						<FavoriteItem>
 							<FavoriteImg />
 							<FavoriteRecipeBox>
 								<FavoriteRecipeTitle>
-									{recipe.title}
+									{favoriteRecipe.title}
 								</FavoriteRecipeTitle>
 								<FavoriteDeleteBtn>icon</FavoriteDeleteBtn>
 								<FavoriteArticle>
-									{recipe.instructions}
+									{favoriteRecipe.instructions}
 								</FavoriteArticle>
-								<FavoriteTime>{recipe.time}</FavoriteTime>
+								<FavoriteTime>
+									{favoriteRecipe.time}
+								</FavoriteTime>
 								<FavoriteSeeBtn>See pecipe</FavoriteSeeBtn>
 							</FavoriteRecipeBox>
 						</FavoriteItem>
 					))}
-				;
 			</FavoriteList>
 		</FavoriteStyled>
 	);
