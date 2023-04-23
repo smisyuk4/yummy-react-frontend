dcdf1b7a7277dadefc14fabb2e24f86d70b9f108
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-04-23 21:28:07 +0300

    Fix pass

--- src/components/Auth/RegisterForm/RegisterForm.jsx
+++ src/components/Auth/RegisterForm/RegisterForm.jsx
@@ -15,7 +15,7 @@ import { FormButton } from 'components/ButtonNav/ButtonNav.styled';
 import { useDispatch } from 'react-redux';
 import { registerUser } from 'redux/auth/operations';
 
-const passwordRegex = /^(?=.*[0-9])(?=.*[A-Z])([a-zA-Z0-9]+)$/;
+const passwordRegex = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/;
 const nameRegex = /^[a-zA-Zа-яА-ЯґҐєЄїЇіІ0-9\s]{6,}$/;
 const emailRegex = /^[\w]{1,64}@([\w]+\.)+[\w]{1,4}$/;
 
