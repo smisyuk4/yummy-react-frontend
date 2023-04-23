4853bd3064a0df001c623cf81a508e778b6bf05e
OlexiiA <incretiblealex@gmail.com>
2023-04-23 16:47:28 +0300

    get render recipes categories

---
+++ src/components/MainPage/MainRecipes/Button/Button.jsx
@@ -0,0 +1,7 @@
+import { ButtonStyled } from './Button.styled';
+
+export const Button = ({ loadMore }) => {
+  return (
+    <ButtonStyled type='button' onClick={loadMore}>Other categories</ButtonStyled>
+  );
+};
