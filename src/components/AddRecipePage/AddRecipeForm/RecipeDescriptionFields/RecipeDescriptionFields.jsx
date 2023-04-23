1e6920b7444651a78a9576848050c043746fc422
Piligrim41m <lightsun41m@gmail.com>
2023-04-23 19:17:28 +0300

    apdate style and logik

--- src/components/AddRecipePage/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
+++ src/components/AddRecipePage/AddRecipeForm/RecipeDescriptionFields/RecipeDescriptionFields.jsx
@@ -4,20 +4,24 @@ import {
 	DescriptionDiv,
 	InputFile,
 	InputForm,
+	InputSelct,
 	InputWrapper,
 	LabelForm,
+	LabelSelect,
 	OverlayPicture,
 	RecipeImg,
 	ThumbImege,
 } from './RecipeDescriptionFields.styled';
 import { useEffect, useState } from 'react';
 import { fetchAllCategory } from 'components/CategoriesPage/FetchWithCategory';
+import { Icon } from 'components/Icon';
 // import { PropTypes } from 'prop-types';
 
-export const RecipeDescriptionFields = ({ onSubmit }) => {
-	const [title, setTitle] = useState('');
-	const [about, setAbout] = useState('');
-	const [categori, setCategori] = useState('');
+export const RecipeDescriptionFields = ({ onChange }) => {
+	// const [title, setTitle] = useState('');
+	// const [about, setAbout] = useState('');
+	// const [categori, setCategori] = useState('');
+	// const [time, setTime] = useState('');
 	const [picture, setPicture] = useState(null);
 
 	const [pictureUrl, setPictureUrl] = useState('');
@@ -36,40 +40,31 @@ export const RecipeDescriptionFields = ({ onSubmit }) => {
 		}
 	}, [picture]);
 
-	console.log('categoiList', categoriList);
-
 	const handleSubmit = async ({ title, about, categori }, { resetForm }) => {
-		console.log(title, about, categori);
-		onSubmit(data);
+		// onSubmit(data);
 		resetForm();
 	};
 
 	const handleOnChange = event => {
-		console.log('Form::onChange', event.target.name);
 		const name = event.target.name;
+		const target = event.target;
+		onChange(target);
 		switch (name) {
-			case 'title':
-				setTitle(event.target.value);
-				console.log('title:', title);
-				break;
-			case 'about':
-				setAbout(event.target.value);
-				console.log('about:', about);
-				break;
+			// 	case 'title':
+			// 		setTitle(event.target.value);
+			// 		break;
+			// 	case 'about':
+			// 		setAbout(event.target.value);
+			// 		break;
 			case 'picture':
 				setPicture(event.target.files[0]);
-				console.log('picture:', picture);
-				// const url = URL.createObjectURL(event.target.value);
-				console.log('picture.value', event.target.files);
-				// console.log('picture', url);
 				break;
-			case 'categori':
-				setCategori(event.target.value);
-				console.log('categori:', categori);
-				break;
-			case 'test':
-				console.log('test select------', event.target.value);
-				break;
+			// 	case 'categori':
+			// 		setCategori(event.target.value);
+			// 		break;
+			// 	case 'time':
+			// 		setTime(event.target.value);
+			// 		break;
 			default:
 				console.log('Invalid subscription type');
 		}
@@ -78,20 +73,23 @@ export const RecipeDescriptionFields = ({ onSubmit }) => {
 		return <option value={categ}>{categ}</option>;
 	});
 
-	const time = [];
+	const timeList = [];
 	for (let i = 5; i <= 120; i += 5) {
-		time.push(i);
+		timeList.push(i);
 	}
-	const timeSelect = time.map(categ => {
-		return <option value={categ}>{categ}</option>;
+	const timeSelect = timeList.map(categ => {
+		return <option value={categ}>{categ} min</option>;
 	});
 
-	const data = {
-		title,
-		about,
-		categori,
-	};
-	console.log('data---', data);
+	// const data = {
+	// 	title,
+	// 	about,
+	// 	categori,
+	// 	picture,
+	// 	time,
+	// };
+	// console.log('data---', data);
+
 	return (
 		<DescriptionDiv>
 			<Formik
@@ -106,65 +104,51 @@ export const RecipeDescriptionFields = ({ onSubmit }) => {
 						<LabelForm>
 							<InputFile name="picture" type="file" multiple />
 							<ThumbImege>
-								<RecipeImg src={pictureUrl} alt="PICTURE" />
+								{pictureUrl ? (
+									<RecipeImg src={pictureUrl} alt="PICTURE" />
+								) : (
+									<Icon
+										id="icon-add-photo-recipe"
+										fill="#fafafa"
+										width={64}
+										height={64}
+									/>
+								)}
 							</ThumbImege>
 						</LabelForm>
 					</OverlayPicture>
 
 					<InputWrapper>
 						<LabelForm>
-							Enter item title
-							<InputForm name="title" type="text" />
-						</LabelForm>
-						<LabelForm>
-							Enter about recipe
-							<InputForm name="about" type="text" />
-						</LabelForm>
-						<LabelForm>
-							Categori
 							<InputForm
+								name="title"
+								type="text"
+								placeholder="Enter item title"
+							/>
+						</LabelForm>
+						<LabelForm>
+							<InputForm
+								name="about"
+								type="text"
+								placeholder="Enter about recipe"
+							/>
+						</LabelForm>
+						<LabelSelect>
+							Categori
+							<InputSelct
 								name="categori"
 								as="select"
 								list="character"
-								multiple>
+								placeholder="Categori">
 								{categoriName}
-
-								{/* <option value=""> </option>
-								<option value="red">Red</option>
-								<option value="green">Green</option>
-								<option value="blue">Blue</option> */}
-							</InputForm>
-						</LabelForm>
-						<div class="wrapper">
-							<select
-								name="test"
-								id=""
-								onTouchStart="this.size=5;"
-								class="form-control"
-								onFocus="this.size=5;"
-								onblur="this.size=1;"
-								onchange="this.size=1; this.blur();">
-								<option value="1">One</option>
-								<option value="2">Two</option>
-								<option value="3">Three</option>
-								<option value="4">Four</option>
-								<option value="5">Five</option>
-								<option value="">Six</option>
-								<option value="">Seven</option>
-								<option value="">Eight</option>
-								<option value="">Nine</option>
-								<option value="">Ten</option>
-							</select>
-						</div>
-						<LabelForm>
+							</InputSelct>
+						</LabelSelect>
+						<LabelSelect>
 							Cooking time
-							<InputForm name="time" as="select">
+							<InputSelct name="time" as="select">
 								{timeSelect}
-								{/* <option value="red">Red</option>
-								<option value="green">Green</option>
-								<option value="blue">Blue</option> */}
-							</InputForm>
-						</LabelForm>
+							</InputSelct>
+						</LabelSelect>
 					</InputWrapper>
 
 					<button type="submit">submit</button>
@@ -186,3 +170,25 @@ export const RecipeDescriptionFields = ({ onSubmit }) => {
 //  - часу приготування рецепту - обирається з випадаючого списку в діапазоні від 5хв до 120хв з кроком 5
 // Випадаючі списки повинні показувати 6 рядків даних, всі інші повинні скролитись в межах даного списку.
 // Компонент пропсами отримує значення для інпутів, а також хендлер для опрацювання цих значень"
+
+/* <div>
+							<select
+								name="test"
+								id=""
+								// class="form-control"
+								// onfocus="this.size=5;"
+								// onblur="this.size=1;"
+								// onChange="this.size=1; this.blur();"
+							>
+								<option value="1">One</option>
+								<option value="2">Two</option>
+								<option value="3">Three</option>
+								<option value="4">Four</option>
+								<option value="5">Five</option>
+								<option value="">Six</option>
+								<option value="">Seven</option>
+								<option value="">Eight</option>
+								<option value="">Nine</option>
+								<option value="">Ten</option>
+							</select>
+						</div> */
