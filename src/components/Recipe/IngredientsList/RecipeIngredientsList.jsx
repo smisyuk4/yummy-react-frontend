e03cde173e31bc1db61a24579d35ad49e52458a6
MOkhten <m.okhten@gmail.com>
2023-04-25 09:12:03 +0300

    recipe description

--- src/components/Recipe/IngredientsList/RecipeIngredientsList.jsx
+++ src/components/Recipe/IngredientsList/RecipeIngredientsList.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { ListBar, DivContainer, ItemBar } from 'components/Shopping/IngredientsShoppingList/IngredientsShoppingListstyled';
-
+// import { getShoppingList } from 'components/Shopping/fetchShoppingList';
 import { IngredientsListSection, ListStyled, IngedientsItem } from './RecipeIngredients.styled';
 
-export const IngredientsList = () => {
+export const IngredientsList = ({ ingredients }) => {
+    
     return (
         <IngredientsListSection>
             <DivContainer>
@@ -15,7 +16,38 @@ export const IngredientsList = () => {
             </DivContainer>
             <>
             <ListStyled>
-                <IngedientsItem>fff</IngedientsItem>
+                    {ingredients &&
+            ingredients.map(({ thb, id, measure, ttl }) => {
+              return (
+                <IngedientsItem key={id}>
+                  <div>
+                    {
+                      <img
+                        src={thb}
+                        alt="Ingredient"
+                      />
+                    }
+                    <p>{ttl}</p>
+                  </div>
+                  <div>
+                    <p>{measure}</p>
+                    {/* <CheckBoxLabel htmlFor={_id}>
+                      <IngedientsCheck
+                        type="checkbox"
+                        checked={!!shoppingList.find(item => item.id === _id)}
+                        id={_id}
+                        value={_id}
+                        onChange={handleInputChange}
+                      />
+                      <CheckBoxWrap>
+                        <CheckMarkIcon />
+                      </CheckBoxWrap>
+                    </CheckBoxLabel> */}
+                  </div>
+                </IngedientsItem>
+              );
+            })}
+        
                 </ListStyled>
                 </>
         </IngredientsListSection>
