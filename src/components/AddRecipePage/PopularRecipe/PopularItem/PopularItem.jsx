bae242b0f4327090bad4419849fed15d3516fb8f
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-04-26 11:51:07 +0300

    write path in popular list and fix path in app routes

--- src/components/AddRecipePage/PopularRecipe/PopularItem/PopularItem.jsx
+++ src/components/AddRecipePage/PopularRecipe/PopularItem/PopularItem.jsx
@@ -1,7 +1,7 @@
 import { PropTypes } from 'prop-types';
+import { NavLink } from 'react-router-dom';
 import {
   LiStyled,
-  AStyled,
   ImgStyled,
   HStyled,
   PStyled,
@@ -10,19 +10,14 @@ import {
 export const PopularItem = ({ _id, title, description, preview }) => {
   return (
     <LiStyled>
-      <AStyled
-        // href={`https://yummy-rest-api.onrender.com/recipes/${_id}`}
-        href='https://www.youtube.com/watch?v=H5TuP6svrpo&ab_channel=%D0%A5%D0%B0%D1%89%D1%96'
-        rel="noopener noreferer"
-        area-label={title}
-      >
+      <NavLink to={`/recipes/${_id}`} replace={true} area-label={title}>
         <ImgStyled src={preview} alt={title} width={100} height={100} />
 
         <div>
           <HStyled>{title}</HStyled>
           <PStyled>{description}</PStyled>
         </div>
-      </AStyled>
+      </NavLink>
     </LiStyled>
   );
 };
