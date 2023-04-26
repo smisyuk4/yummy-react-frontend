6e0a997f8c1539c7b78a962abcab8e0ab47601cd
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2023-04-26 10:35:08 +0300

    added logic

---
+++ src/components/NoResults/NoResults.styled.js
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	text-align: center;
+	padding-bottom: 100px;
+	@media ${props => props.theme.media.desktop} {
+		padding-bottom: 200px;
+	}
+	@media ${props => props.theme.media.desktop} {
+		padding-bottom: 200px;
+	}
+`;
+
+export const ImgWrapper = styled.div`
+	width: 208px;
+	margin: 0 auto 24px auto;
+	@media ${props => props.theme.media.tablet} {
+		width: 350px;
+		margin-bottom: 32px;
+	}
+`;
+
+export const NoResImg = styled.img`
+	width: 100%;
+`;
+
+export const Text = styled.p`
+	font-weight: 500;
+	font-size: 14px;
+	line-height: 100px;
+	letter-spacing: -0.02em;
+
+	color: #000000;
+
+	opacity: 0.5;
+
+	@media ${props => props.theme.media.tablet} {
+		font-size: 24px;
+	}
+	@media ${props => props.theme.media.desktop} {
+		padding-bottom: 200px;
+	}
+`;
--- src/components/OwnRecipes/OwnRecipes.jsx
+++ src/components/OwnRecipes/OwnRecipes.jsx
@@ -1,48 +1,50 @@
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { RecipeList } from 'components/RecipeList';
 import { RecipeListItem } from 'components/RecipeListItem';
-
-// import { deleteRecipe, fetchOwnRecipes } from './operations';
-
-// export const OwnRecipes = () => {
-// 	const [ownRecipes, setOwnRecipes] = useState();
-
-// 	useEffect(() => {
-// 		async function fetch() {
-// 			try {
-// 				const ownRecipes = await fetchOwnRecipes();
-// 				setOwnRecipes(prev => ownRecipes.data.data.recipes);
-// 			} catch (error) {
-// 				console.log(error);
-// 			} finally {
-// 			}
-// 		}
-
-// 		fetch();
-// 	}, []);
-
-// 	if (ownRecipes.length > 0) {
-// 		return (
-// 			<RecipeList>
-// 				{ownRecipes.map(recipe => (
-// 					<RecipeListItem
-// 						key={recipe.id}
-// 						recipe={recipe}
-// 						onDelete={deleteRecipe}
-// 					/>
-// 				))}
-// 				<RecipeListItem />
-// 			</RecipeList>
-// 		);
-// 	}
-// };
+import { NoResults } from 'components/NoResults';
+import { deleteOwnRecipe, fetchOwnRecipes } from './operations';
 
 export const OwnRecipes = () => {
-	return (
-		<RecipeList>
-			<RecipeListItem />
-			<RecipeListItem />
-		</RecipeList>
-	);
+	const [ownRecipes, setOwnRecipes] = useState();
+
+	useEffect(() => {
+		async function fetch() {
+			try {
+				const ownRecipes = await fetchOwnRecipes();
+				setOwnRecipes(prev => ownRecipes.data.recipes);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+
+		fetch();
+	}, []);
+
+	async function deleteRecipe(id) {
+		try {
+			await deleteOwnRecipe(id);
+			const updatedRecipesList = await fetchOwnRecipes();
+			console.log('updated', updatedRecipesList);
+			setOwnRecipes(prev => updatedRecipesList.data.recipes);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	if (ownRecipes?.length > 0) {
+		return (
+			<RecipeList>
+				{ownRecipes.map(recipe => (
+					<RecipeListItem
+						key={recipe._id}
+						recipe={recipe}
+						onDelete={() => deleteRecipe(recipe._id)}
+					/>
+				))}
+			</RecipeList>
+		);
+	} else {
+		return <NoResults text="No recipes found" />;
+	}
 };
