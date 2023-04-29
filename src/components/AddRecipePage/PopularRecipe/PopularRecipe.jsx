375196447cbc18b9a36fba861415118d1db2df40
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-04-29 17:48:52 +0300

    show code popular recipes

--- src/components/AddRecipePage/PopularRecipe/PopularRecipe.jsx
+++ src/components/AddRecipePage/PopularRecipe/PopularRecipe.jsx
@@ -36,36 +36,36 @@ export const PopularRecipe = ({ title }) => {
 
   const count = windowWidth <= 767 ? 4 : windowWidth <= 1439 ? 2 : 4;
 
-  // if (popRecipes?.length === 0) {
+  if (popRecipes?.length === 0) {
     return (
       <DivStyled>
         <Title title={title} className={'title-popular'} />
         <p>Sorry, but we are still preparing the collection for you</p>
       </DivStyled>
     );
-  // }
+  }
 
-  // if (popRecipes?.length > 0) {
-  //   return (
-  //     <DivStyled>
-  //       <Title title={title} className={'title-popular'} />
+  if (popRecipes?.length > 0) {
+    return (
+      <DivStyled>
+        <Title title={title} className={'title-popular'} />
 
-  //       <UlStyled>
-  //         {popRecipes
-  //           .slice(0, count)
-  //           .map(({ _id, title, description, preview }) => (
-  //             <PopularItem
-  //               key={_id}
-  //               _id={_id}
-  //               title={title}
-  //               description={description}
-  //               preview={preview}
-  //             />
-  //           ))}
-  //       </UlStyled>
-  //     </DivStyled>
-  //   );
-  // }
+        <UlStyled>
+          {popRecipes
+            .slice(0, count)
+            .map(({ _id, title, description, preview }) => (
+              <PopularItem
+                key={_id}
+                _id={_id}
+                title={title}
+                description={description}
+                preview={preview}
+              />
+            ))}
+        </UlStyled>
+      </DivStyled>
+    );
+  }
 };
 
 PopularRecipe.propTypes = {
