7a4d9613792e6494e4c79d89dd02b9840bb4ff87
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-04-29 19:38:19 +0300

    clear console.log

--- src/components/SearchingPage/Searchingbar/SearchingBar.jsx
+++ src/components/SearchingPage/Searchingbar/SearchingBar.jsx
@@ -1,50 +1,47 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import {
-	FormStyled,
-	SearchStyled,
-	SearchFormBtn,
-	Input,
+  FormStyled,
+  SearchStyled,
+  SearchFormBtn,
+  Input,
 } from './SearchingBar.styled';
 
 export const SearchingBar = ({ changeValue }) => {
-	const [searchValue, setsearchValue] = useState('');
-	const { query } = useParams();
-	useEffect(() => {
-		if (query) {
-			setsearchValue(query);
-		}
-	}, [query]);
+  const [searchValue, setsearchValue] = useState('');
+  const { query } = useParams();
+  useEffect(() => {
+    if (query) {
+      setsearchValue(query);
+    }
+  }, [query]);
 
-	const handleSubmit = event => {
-		event.preventDefault();
-		changeValue(searchValue);
-		// setsearchValue('');
-	};
+  const handleSubmit = event => {
+    event.preventDefault();
+    changeValue(searchValue);
+  };
 
-	const inputChange = event => {
-		setsearchValue(event.target.value);
-	};
+  const inputChange = event => {
+    setsearchValue(event.target.value);
+  };
 
-	return (
-		<SearchStyled>
-			<FormStyled onSubmit={handleSubmit}>
-				<div className="position">
-					<Input
-						name="search"
-						type="text"
-						autoComplete="off"
-						autoFocus
-						value={searchValue}
-						onChange={inputChange}
-						// pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-						// title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-					/>
-					<SearchFormBtn type="submit">
-						<span>Search</span>
-					</SearchFormBtn>
-				</div>
-			</FormStyled>
-		</SearchStyled>
-	);
+  return (
+    <SearchStyled>
+      <FormStyled onSubmit={handleSubmit}>
+        <div className="position">
+          <Input
+            name="search"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            value={searchValue}
+            onChange={inputChange}
+          />
+          <SearchFormBtn type="submit">
+            <span>Search</span>
+          </SearchFormBtn>
+        </div>
+      </FormStyled>
+    </SearchStyled>
+  );
 };
