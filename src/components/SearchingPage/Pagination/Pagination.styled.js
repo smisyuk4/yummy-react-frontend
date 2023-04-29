005a95862ef688271f8ea36b9d66653aac9e863a
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2023-04-30 01:48:59 +0200

    styled_pagination_all

--- src/components/SearchingPage/Pagination/Pagination.styled.js
+++ src/components/SearchingPage/Pagination/Pagination.styled.js
@@ -2,20 +2,28 @@ import styled from '@emotion/styled';
 
 export const DivStyled = styled.div`
 	position: relative;
-	box-shadow: 0px 4px 4px rgba(135, 135, 135, 0.2);
+	box-shadow: 0px 4px 4px ${({ theme }) => theme.colors.paginationBG};
 	border-radius: 26px;
-	background-color: #fafafa;
+	background-color: ${({ theme }) => theme.colors.bgModal};
 	display: inline-flex;
 	z-index: 100;
-	/* width: 275px; */
+	height: 51px;
 	justify-content: space-around;
 	align-items: center;
+
+	@media screen and (min-width: 768px) {
+		height: 55px;
+	}
+	@media screen and (min-width: 1440px) {
+		height: 60px;
+	}
 
 	& .pagination {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		padding: 0 10px;
+		color: ${({ theme }) => theme.colors.recipeTextColor};
 	}
 	& .pagination li {
 		list-style: none;
@@ -31,16 +39,16 @@ export const DivStyled = styled.div`
 		align-items: center;
 		text-decoration: none;
 		cursor: pointer;
-		padding: 8px 16px;
+		padding: 6px 12px;
 		border-radius: 50%;
 	}
 	& .pagination li a:hover {
-		color: #22252a;
-		background-color: #ebf3d4;
+		color: ${({ theme }) => theme.colors.fontHero};
+		background-color: ${({ theme }) => theme.colors.paginationBGActive};
 	}
 	& .pagination li.active a {
-		color: #656565;
-		background-color: #ebf3d4;
+		color: ${({ theme }) => theme.colors.fontHero};
+		background-color: ${({ theme }) => theme.colors.paginationBGActive};
 		/* width: 27px;
 		height: 27px; */
 		border-radius: 50%;
