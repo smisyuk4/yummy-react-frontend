37c17034425a70d4aab2c31feda529ded0c16eb1
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-04-29 23:13:58 +0300

    add loader to favorite

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,84 +1,93 @@
 import { useEffect, useState } from 'react';
+import { ColorRing } from 'react-loader-spinner';
 import { fetchFavorite, removeFromFavorites } from './FetchFavorite';
 
 import {
-	FavoriteArticle,
-	FavoriteDeleteBtn,
-	FavoriteIcon,
-	FavoriteImg,
-	FavoriteImgBox,
-	FavoriteItem,
-	FavoriteList,
-	FavoriteRecipeBox,
-	FavoriteRecipeTitle,
-	FavoriteSeeBtn,
-	FavoriteStyled,
-	FavoriteTime,
+  FavoriteArticle,
+  FavoriteDeleteBtn,
+  FavoriteIcon,
+  FavoriteImg,
+  FavoriteImgBox,
+  FavoriteItem,
+  FavoriteList,
+  FavoriteRecipeBox,
+  FavoriteRecipeTitle,
+  FavoriteSeeBtn,
+  FavoriteStyled,
+  FavoriteTime,
 } from './Favorite.styled';
 
 import { ReusableComponentTitleWithJewelry } from 'components/ReusableComponentTitleWithJewelry';
 
 export const Favorite = id => {
-	const [favoriteRecipes, setFavoriteRecipes] = useState();
+  const [favoriteRecipes, setFavoriteRecipes] = useState();
 
-	useEffect(() => {
-		async function fetch() {
-			try {
-				const favorit = await fetchFavorite();
+  useEffect(() => {
+    async function fetch() {
+      try {
+        const favorit = await fetchFavorite();
 
-				setFavoriteRecipes(prev => favorit.data.data.recipes);
-			} catch (error) {
-				console.log(error);
-			} finally {
-			}
-		}
+        setFavoriteRecipes(prev => favorit.data.data.recipes);
+      } catch (error) {
+        console.log(error);
+      } finally {
+      }
+    }
 
-		fetch();
-	}, []);
+    fetch();
+  }, []);
 
-	const onDelete = async id => {
-		await removeFromFavorites(id);
-		setFavoriteRecipes(prev => prev.filter(recipe => recipe._id !== id));
-	};
+  const onDelete = async id => {
+    await removeFromFavorites(id);
+    setFavoriteRecipes(prev => prev.filter(recipe => recipe._id !== id));
+  };
 
-	return (
-		<FavoriteStyled>
-			<ReusableComponentTitleWithJewelry title="Favorites" />
+  return (
+    <FavoriteStyled>
+      <ReusableComponentTitleWithJewelry title="Favorites" />
 
-			<FavoriteList>
-				{favoriteRecipes?.length > 0 &&
-					favoriteRecipes.map(
-						({ _id, title, instructions, time, thumb }) => (
-							<FavoriteItem key={_id}>
-								<FavoriteImgBox>
-									<FavoriteImg src={thumb} />
-								</FavoriteImgBox>
+      <FavoriteList>
+        {!favoriteRecipes ? (
+          <ColorRing
+            visible={true}
+            ariaLabel="blocks-loading"
+            wrapperClass="blocks-wrapper"
+            colors={['#2a2c36', '#f43e60', '#FFBC00', '#89BC24', '#B8444A']}
+          />
+        ) : (
+          <>
+            {favoriteRecipes?.map(
+              ({ _id, title, instructions, time, thumb }) => (
+                <FavoriteItem key={_id}>
+                  <FavoriteImgBox>
+                    <FavoriteImg src={thumb} />
+                  </FavoriteImgBox>
 
-								<FavoriteRecipeBox>
-									<FavoriteRecipeTitle>
-										{title}
-									</FavoriteRecipeTitle>
-									<FavoriteDeleteBtn
-										type="button"
-										onClick={() => onDelete(_id)}>
-										<FavoriteIcon
-											id="icon-trash"
-											fill="#EBF3D4"
-											width={11}
-											height={12}></FavoriteIcon>
-									</FavoriteDeleteBtn>
-									<FavoriteArticle>
-										{instructions}
-									</FavoriteArticle>
-									<FavoriteTime>{time} min</FavoriteTime>
-									<FavoriteSeeBtn to={`/recipes/${_id}`}>
-										See pecipe
-									</FavoriteSeeBtn>
-								</FavoriteRecipeBox>
-							</FavoriteItem>
-						)
-					)}
-			</FavoriteList>
-		</FavoriteStyled>
-	);
+                  <FavoriteRecipeBox>
+                    <FavoriteRecipeTitle>{title}</FavoriteRecipeTitle>
+                    <FavoriteDeleteBtn
+                      type="button"
+                      onClick={() => onDelete(_id)}
+                    >
+                      <FavoriteIcon
+                        id="icon-trash"
+                        fill="#EBF3D4"
+                        width={11}
+                        height={12}
+                      ></FavoriteIcon>
+                    </FavoriteDeleteBtn>
+                    <FavoriteArticle>{instructions}</FavoriteArticle>
+                    <FavoriteTime>{time} min</FavoriteTime>
+                    <FavoriteSeeBtn to={`/recipes/${_id}`}>
+                      See pecipe
+                    </FavoriteSeeBtn>
+                  </FavoriteRecipeBox>
+                </FavoriteItem>
+              )
+            )}
+          </>
+        )}
+      </FavoriteList>
+    </FavoriteStyled>
+  );
 };
