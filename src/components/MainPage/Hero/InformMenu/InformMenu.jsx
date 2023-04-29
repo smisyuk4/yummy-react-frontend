6b0c654d80769f64d1095add9409f0d9e83e592c
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-04-29 22:44:53 +0300

    change path for all recipes

--- src/components/MainPage/Hero/InformMenu/InformMenu.jsx
+++ src/components/MainPage/Hero/InformMenu/InformMenu.jsx
@@ -8,7 +8,7 @@ import { InformWrapperStyled,
 export const InformMenu = () => {
   return <InformWrapperStyled>
   <TextfStyled><span style={{color: '#8BAA36'}}>Delicious and healthy</span> way to enjoy a variety of fresh ingredients in one satisfying meal</TextfStyled>
-  <BtnWrapperStyled to="/categories/Beef">
+  <BtnWrapperStyled to="/categories/Breakfast">
     <BtnTextStyled>See all recipes</BtnTextStyled>
     <IconStyled><Icon id="icon-arrow-right" height="18" width="22" /></IconStyled>
   </BtnWrapperStyled>
