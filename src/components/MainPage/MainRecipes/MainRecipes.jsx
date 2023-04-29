a52453e3169521e54bbd54238c47547de7aabe30
OlexiiA <incretiblealex@gmail.com>
2023-04-29 15:20:53 +0300

    Fix bag on scroll and arr

--- src/components/MainPage/MainRecipes/MainRecipes.jsx
+++ src/components/MainPage/MainRecipes/MainRecipes.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { fetchRecipes } from './FetchRecipes';
 import { NavLink } from 'react-router-dom';
@@ -20,9 +20,8 @@ import {
 export const MainRecipes = () => {
 	const [recipes, setRecipes] = useState([]);
 	const [displayedRecipesCount, setDisplayedRecipesCount] = useState(4);
-	const [windowWidth, setWindowWidth] = useState(
-		() => window.innerWidth || 0
-	);
+	const [windowWidth, setWindowWidth] = useState(() => window.innerWidth || 0	);
+	const loadMoreButtonRef = useRef(null); //не вызывает повторный рендеринг кнопки.
 
 	useEffect(() => {
 		fetchRecipes()
@@ -43,27 +42,32 @@ export const MainRecipes = () => {
 		window.addEventListener('resize', handleResize);
 		return () => window.removeEventListener('resize', handleResize);
 	}, []);
-	// console.log(`Ширина экрана: ${windowWidth}px`);
+
+	//loadMoreButtonRef.current.scrollIntoView делает скролл после нажатия кнопки, чтобы скроллилось при загрузке к нужному елементу, на елемент добавил ref={loadMoreButtonRef}! 
 	const loadMore = () => {
 		setDisplayedRecipesCount(prevCount => prevCount + 4);
-	};
-//!!trottle
+		loadMoreButtonRef.current.scrollIntoView({
+		  behavior: 'smooth',
+		  block: 'start'
+		});
+	  };
+
 	const count = windowWidth <= 767 ? 1 : windowWidth <= 1439  ? 2  : 4;
 
 	return (
-		<Section>
-			<ul>
+		<Section >
+			<ul >
 				{recipes.slice(0, displayedRecipesCount).map(oneRes => {
 					const { category } = oneRes[0];
 					return (
-						<li key={uuidv4()} >
-							<TitleCategory>{category}</TitleCategory>
+						<li key={uuidv4()} ref={loadMoreButtonRef}>
+							<TitleCategory >{category}</TitleCategory>
 							<CardItem>
 								{oneRes
 									.slice(0, count)
 									.map(({ title, thumb, _id }) => {
 										return (
-											<ImageBox key={uuidv4()}>
+											<ImageBox key={uuidv4()} >
 											<NavLink to={`/recipes/${_id}`}>
 												<Image
 													src={thumb}
@@ -87,12 +91,12 @@ export const MainRecipes = () => {
 						</li>
 					);
 				})}
-			</ul>
-			{displayedRecipesCount < recipes.length && (
-				<BtnBox>
-					<Button loadMore={loadMore}>Load More</Button>
-				</BtnBox>
-			)}
+			</ul>			
+				{displayedRecipesCount < recipes.length && (
+					<BtnBox>
+						<Button loadMore={loadMore}>Load More</Button>
+					</BtnBox>
+				)}			
 		</Section>
 	);
 };
