8a90ad6e3f835ab8ee6fa99c5779d8535afdc713
MOkhten <m.okhten@gmail.com>
2023-05-01 09:52:32 +0300

    split for recipe description

--- src/components/Recipe/RecipePreparation/RecipePraparation.jsx
+++ src/components/Recipe/RecipePreparation/RecipePraparation.jsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import {PreparationSection, ImageWrapper, InstructionItem, TextDiv, ImgStyled, TitleStyles} from './RecipePreparation.styled'
 import photo from 'images/recipeImg/ingredImage.jpg'
 
-export const RecipePreparation = ({instructions, thumb}) => {
+export const RecipePreparation = ({ instructions, thumb }) => {
+    let items = [];
+    if (instructions) {
+    items = instructions
+      .split('\r\n')
+      .filter(el => {
+        if (!el) return false;
+        if (!isNaN(el)) return false;
+        if (el.toLowerCase().includes('step')) return false;
+          return true;
+      })
+      .map((item, index) => {
+        let splitItem = item;
+        for (let i = 0; i < 2; i++) {
+          if (!isNaN(splitItem[i]) ||splitItem[i] === '.') {
+            splitItem = splitItem.slice(i + 1);
+          }
+        }
+        return (
+          <InstructionItem key={uuidv4()}>
+            <div>{index + 1}</div>
+            <p>{splitItem}</p>
+          </InstructionItem>
+        );
+      });
+  }
+ 
     return (
         <PreparationSection>
             <TextDiv>
             <TitleStyles>Recipe Preparation</TitleStyles>
             <ol>
-                <InstructionItem>{ instructions}</InstructionItem>
+                { items}
                 </ol>
                 </TextDiv>
             <ImageWrapper>
--- src/components/Recipe/RecipePreparation/RecipePreparation.styled.js
+++ src/components/Recipe/RecipePreparation/RecipePreparation.styled.js
@@ -37,8 +37,8 @@ export const TitleStyles = styled.h2`
 
 export const InstructionItem = styled.li`
   display: flex;
-   white-space: pre-wrap;
-  align-items: center;
+   /* white-space: pre-wrap; */
+  align-items: flex-start;
     margin-bottom: 18px;
     font-size: 12px;
     color: ${({ theme }) => theme.colors.recipeColor};
@@ -46,6 +46,28 @@ export const InstructionItem = styled.li`
   letter-spacing: -0.02em;
   @media ${props => props.theme.media.tablet} {
     font-size: 14px;
+  }
+  & div{
+width: 21px;
+height: 21px;
+border-radius: 100px;
+/* padding: 2px 7px 1px; */
+gap: 10px;
+width: 21px;
+font-weight: 600;
+font-size: 12px;
+line-height: 18px;
+background: ${({ theme }) => theme.colors.primary};
+color: #FFFFFF;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+isolation: isolate;
+margin-right: 14px;
+flex: none;
+order: 0;
+flex-grow: 0;
   }
 `;
 
